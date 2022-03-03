2c0a6a59b22a160842c3fc70c21a3f84dd6b5965
Hugo Nicolás Leyton Díaz <hleyton@alumnos.inf.utfsm.cl>
2022-03-03 02:13:54 -0300

    Completed tasks: - Added functionality for code submission. - Improved interface for exercises.

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import jsonify
 from flaskext.markdown import Markdown
 from flaskext.mysql import MySQL
 from datetime import datetime
+from time import sleep
+import requests
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -28,6 +31,9 @@ categories = [
     ['tuplas', 'Tuplas'],
     ['diccionarios', 'Diccionarios']
 ]
+ip_judge0 = '18.118.22.172'
+port_judge0 = '2358'
+url_judge0 = 'http://' + ip_judge0 + ':' + port_judge0
 
 def check_quiz_attempted(cursor, email, category):
     cursor.execute('SELECT COUNT(*) FROM quiz_attempts WHERE email = %(email)s AND category = %(cat)s;', { 'email': email, 'cat': category})
@@ -129,3 +135,70 @@ def exercises(category=None, id_exercise=None):
             'Ejemplo 4'
         ]
         return render_template('exercises.html', exercises=exercises, exercise_category=category, quiz_attempted=quiz_attempted)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    body = request.get_json()
+    
+    if body is None:
+        return 'The request body is null', 400
+    
+    if 'id_problem' not in body:
+        return 'You need to specify the id_problem', 400
+    
+    if 'source_code' not in body:
+        return 'You need to specify the source_code', 400
+
+    id_problem = body['id_problem']
+    source_code = body['source_code']
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('SELECT id_test_case, stdin, expected_output, feedback, sample FROM test_cases WHERE id_problem=%(id_pro)s AND subproblem=1 ORDER BY sample DESC, id_problem ASC;', { 'id_pro': id_problem})
+    tests_cases = cursor.fetchall()
+
+    data = {
+        "submissions": []
+    }
+
+    for i in range(len(tests_cases)):
+        _, stdin, expected_output, _, _ = tests_cases[i]
+        payload = {
+            "language_id": 71,
+            "source_code": source_code,
+            "stdin": stdin,
+            "expected_output": expected_output
+        }
+        data["submissions"].append(payload)
+
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36'}
+        response = requests.post(url_judge0 + '/submissions/batch', json=data, headers=headers)
+        tokens = response.json()
+        tokens_str = ''
+        
+        for i in range(len(tokens)):
+            tokens_str += tokens[i]['token']
+            tokens_str += ',' if i < len(tokens) - 1 else ''
+        
+        fields = 'stdin,stdout,stderr,status_id,token'
+
+        while True:
+            response = requests.get(url_judge0 + '/submissions/batch?tokens=' + tokens_str + '&base64_encoded=false&fields=' + fields)
+            data = response.json()["submissions"]
+            cont = 0
+            for i in range(len(data)):
+                status_id = data[i]['status_id']
+                cont += int(status_id == 1 or status_id == 2)
+            if cont == 0:
+                break
+            else:
+                sleep(2)
+
+        return response.json(), 200
+    except:
+        return 'Judgment error', response.status_code
+
+
+
+
